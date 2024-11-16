3abca78b9760ab81637041d8d94216965ed10622
DeltaBeee <niconez04@icloud.com>
2024-11-16 17:29:29 -0500

    Init

---
+++ src/main/java/net/deltabeee/alchemicalwarfare/AlchemicalWarfareClient.java
@@ -0,0 +1,11 @@
+package net.deltabeee.alchemicalwarfare;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlchemicalWarfareClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
